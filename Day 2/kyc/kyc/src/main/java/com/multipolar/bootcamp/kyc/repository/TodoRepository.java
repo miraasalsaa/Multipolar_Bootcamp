a4008f1c11967c2d99844caa2735b62acdd0f7ff
miraasalsaa <147487756+miraasalsaa@users.noreply.github.com>
2023-10-13 08:09:51 +0700

    Delete Day 2/kyc directory

--- Day 2/kyc/kyc/src/main/java/com/multipolar/bootcamp/kyc/repository/TodoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.multipolar.bootcamp.kyc.repository;
-
-import com.multipolar.bootcamp.kyc.domain.Todo;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TodoRepository extends MongoRepository<Todo,String> {
-
-}
